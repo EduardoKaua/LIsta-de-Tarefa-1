fca66b0f9b3d467b7daca3333d07f8c221a28376
Eduardo Kauã de Souza <edubr038@gmail.com>
2021-11-30 15:59:48 -0300

    Fazendo JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const input = document.getElementById("input");
+const ul = document.getElementById("ul");
+const tarefa = [];
 
+function add() {
+  tarefa.push(input.value);
+  input.value = "";
+  render();
+}
+function render() {
+  ul.innerHTML = null;
+  tarefa.forEach((i) => {
+    const li = document.createElement("li");
+    li.innerText = i;
+    ul.appendChild(li);
+  });
+}
