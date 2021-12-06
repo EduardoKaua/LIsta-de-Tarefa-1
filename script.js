2b43fc46b0bb341dc3dc373e2f0450ebe6cf7470
Eduardo Kauã de Souza <edubr038@gmail.com>
2021-12-06 16:01:45 -0300

    arrumando css

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ const ul = document.getElementById("ul");
 const tarefa = [];
 
 function add() {
-  if (input.value != "") tarefa.push(input.value);
-  input.value = "";
-  render();
+  if (!input.value) {
+    alert("Sem tarefa para adicionar");
+  } else {
+    tarefa.push(input.value);
+    input.value = "";
+    render();
+  }
 }
 function render() {
   ul.innerHTML = null;
